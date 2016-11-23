5e82756c4463f624e7e3aa10c4cdc8b54780962c
Donal Maher <dpjm94@live.ie>
2016-11-23 13:07:19 +0000

    Added New code for Job
    In the Job class:
    Added following fields: job_id, category, location, employType
    Added getJob_id method
    Added getCategory method
    Added getLocation method
    Added getEmployType method
    Added setCategory method
    Added setLocation method
    Added setEmployType method

--- src/Job.java
+++ src/Job.java
@@ -2,4 +2,46 @@
  * Created by donalmaher on 23/11/2016.
  */
 public class Job {
+
+    private String job_id;
+    private String category;
+    private String location;
+    private String employType;
+
+
+    public Job(String job_id, String category, String location, String employType) {
+        this.job_id = job_id;
+        this.category = category;
+        this.location = location;
+        this.employType = employType;
+    }
+
+
+    public String getJob_id() {
+        return job_id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getEmployType() {
+        return employType;
+    }
+
+    public void setEmployType(String employType) {
+        this.employType = employType;
+    }
 }
