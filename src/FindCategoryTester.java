bd5a0aac8be3ba379bc09850b13c8f0864185dd0
Donal Maher <dpjm94@live.ie>
2016-11-23 21:03:01 +0000

    Added the code in Inventory class
    Added private List jobs
    Added addJob method in Inventory
    Added getJob method in Inventory
    Added search job method in Inventory
    
    file: FindCategoryTester
    Added main method
    Added initializeInventory method
    Added one value in initializeInventory method

--- src/FindCategoryTester.java
+++ src/FindCategoryTester.java
@@ -2,4 +2,31 @@
  * Created by donalmaher on 23/11/2016.
  */
 public class FindCategoryTester {
+
+    public static void main(String args[]){
+
+        Inventory inventory = new Inventory();
+        initializeInventory(inventory);
+
+        //Customer looking for Accounting in Dublin, only parttime job
+        Job whatCustomerWant = new Job("01","Accounting","Dublin","PartTime");
+
+        Job job = inventory.search(whatCustomerWant);
+        if(job != null){
+            System.out.println(" Customer, You might like these job in the following: " +
+            job.getCategory() + " in " + job.getLocation() + ". " +
+            " You must be " + job.getEmployType());
+
+        }
+        else{
+            System.out.println("Sorry Customer, there is no matching job what you looking for");
+        }
+    }
+
+    private static void initializeInventory(Inventory inventory) {
+
+       inventory.addJob("01","Accounting","Dublin","PartTime");
+    }
+
+
 }
--- src/Inventory.java
+++ src/Inventory.java
@@ -1,5 +1,54 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by donalmaher on 23/11/2016.
  */
 public class Inventory {
-}
+
+    private List jobs;
+
+        public Inventory() {
+            jobs = new LinkedList();
+        }
+
+
+        public void addJob(String job_id,String category, String location, String employType){
+            Job job = new Job(job_id, category,location,employType);
+            jobs.add(job);
+        }
+
+
+        public Job getJob(String job_id){
+            for(Iterator i = jobs.iterator(); i.hasNext();){
+                Job job = (Job)i.next();
+                if(job.getJob_id().equals(job_id)){
+                    return job;
+                }
+            }
+            return null;
+        }
+
+        public Job search(Job searchJob){
+            for(Iterator i = jobs.iterator(); i.hasNext();){
+                Job job = (Job)i.next();
+
+                String category = searchJob.getCategory();
+                if((category != null) && (!category.equals("")) &&
+                        (!category.equalsIgnoreCase(job.getCategory())))
+                    continue;
+
+                String location = searchJob.getLocation();
+                if((location != null) && (!location.equals("")) &&
+                        (!location.equals(job.getLocation())))
+                    continue;
+
+                String employType = searchJob.getEmployType();
+                if((employType != null) && (!employType.equals("")) &&
+                        (!category.equals(job.getEmployType())))
+                    continue;
+            }
+            return null;
+        }
+    }
