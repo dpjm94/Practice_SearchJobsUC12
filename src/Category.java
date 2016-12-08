f99c0e6ccc4f1e26096d12fb1aae350c051ea149
Donal Maher <dpjm94@live.ie>
2016-12-08 12:35:49 +0000

    Changes in Category.java
    Modfiy HR to HN

--- src/Category.java
+++ src/Category.java
@@ -1,7 +1,7 @@
 public enum Category {
 
     ACCOUNTING, ARCHITECTURE, ARTS, ENTERTAINMENT, CONSTRUCTION,
-    EDUCATION, ENGINEERING, BANKING, HOTEL, HR, INSURANCE,
+    EDUCATION, ENGINEERING, BANKING, HOTEL, HN, INSURANCE,
     IT, MARKETING, SALES, SECURITY, SCIENCE, SPORT, TECHNICAL;
 
     public String toString() {
@@ -15,7 +15,7 @@ public enum Category {
             case ENGINEERING:    return "Engineering";
             case BANKING :     return "Engineering";
             case HOTEL :     return "Hotel";
-            case HR :     return "Hr";
+            case HN:     return "HN";
             case INSURANCE :     return "Insurance";
             case MARKETING :     return "Marketing";
             case SALES :     return "Sales";
